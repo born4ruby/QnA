821cc3d3a247ea9a228a877fac68b693715a2758
Jason Rao <born4ruby@gmail.com>
2014-02-13 15:48:14 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QnA::Application.config.secret_token = '64001c667d03cfef73525f4f3be4fa63c732093a5ceebafed4a5f47c7a12d3431d0fdd23ef42ee20114ba8fd14de98800c231a14af3f2b28cd3bc208e774d172'
